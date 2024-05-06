b511560eb9843cc36485559e08f114db44910f4d
David Sukhashvili <davidsukhashvili@MacBook-Pro-David.local>
2024-05-06 22:22:45 +0300

    add cache

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState } from "react";
 import "./App.css";
 import { useFetchData } from "./hooks/useFetchData";
 
@@ -11,29 +10,66 @@ interface Todo {
 }
 
 function App() {
-  const fetchFunction = () =>
-    fetch("https://jsonplaceholder.typicode.com/todos/1").then((response) =>
-      response.json()
-    );
+  const [todos, setTodos] = useState<string[]>([]);
+  const [input, setInput] = useState("");
 
-  const { data, reloadFetch } = useFetchData<Todo>({ fetchFunction });
-  console.log(data);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
       <div className="card">
-        <button onClick={reloadFetch}>Reload</button>
+        <button
+          onClick={() => {
+            const indexValue = input
+              ? input
+              : `${Math.floor(Math.random() * 100)}`;
+
+            setInput("");
+
+            setTodos((prev) => [...prev, indexValue]);
+          }}
+        >
+          Add New Todo
+        </button>
+        <div style={{ display: "flex", paddingTop: 50, gap: 10 }}>
+          {todos.map((el, i) => {
+            return <TodoView indexTodo={parseInt(el)} key={el + i} />;
+          })}
+        </div>
       </div>
     </>
   );
 }
 
 export default App;
+
+function TodoView({ indexTodo }: { indexTodo: number }) {
+  const fetchTodos = (init?: RequestInit) =>
+    fetch(`https://jsonplaceholder.typicode.com/todos/${indexTodo}`, init).then(
+      (response) => response.json()
+    );
+
+  const { data, reloadFetch, isLoading } = useFetchData<Todo>({
+    fetchFunction: fetchTodos,
+    nameForCache: "fetchTodos" + indexTodo,
+  });
+  // console.log(data);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div style={{ border: "1px solid lightgreen", padding: 10 }}>
+      <div>
+        Todo:
+        <div>id: {data?.id}</div>
+        <div>title: {data?.title}</div>
+        <div>completed: {data?.completed}</div>
+      </div>
+      <div className="card">
+        <button onClick={reloadFetch}>Reload</button>
+      </div>
+    </div>
+  );
+}
--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -6,10 +6,14 @@ import {
   useState,
 } from "react";
 
+const cache = new Map();
+
 export const useFetchData = <T>({
   fetchFunction,
+  nameForCache,
 }: {
-  fetchFunction: () => Promise<T>;
+  fetchFunction: (init?: RequestInit) => Promise<T>;
+  nameForCache?: string;
 }) => {
   const memoizedFn = useRef(fetchFunction);
   const [data, setData] = useState<T | null>(null);
@@ -17,26 +21,46 @@ export const useFetchData = <T>({
   const [errors, setError] = useState<Error | null>(null);
   const [reload, setReload] = useState(false);
 
+  //update data
+  const [refetch, setRefetch] = useState(false);
+
   useLayoutEffect(() => {
     memoizedFn.current = fetchFunction;
   }, [fetchFunction]);
 
   const reloadFetch = useCallback(() => {
+    setRefetch(true);
     setReload((prev) => !prev);
   }, []);
 
   useEffect(() => {
+    //add to cache
+    if (cache.has(nameForCache) && !refetch) {
+      const data = cache.get(nameForCache);
+      setData(data);
+      return;
+    }
+
     setIsLoading(true);
     setError(null);
 
+    const controller = new AbortController();
+
     memoizedFn
-      .current()
+      .current({ signal: controller.signal })
       .then((res) => {
         setData(res);
         setIsLoading(false);
+
+        cache.set(nameForCache, res);
+        setRefetch(false);
       })
       .catch(setError);
-  }, [reload]);
+
+    return () => {
+      controller.abort();
+    };
+  }, [nameForCache, refetch, reload]);
 
   return { data, isLoading, errors, reloadFetch };
 };
