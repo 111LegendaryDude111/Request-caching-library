0d5a66bccfa15841963fc1aad11441091c00ce7b
David Sukhashvili <davidsukhashvili@MacBook-Pro-David.local>
2024-06-17 13:37:54 +0300

    add new api

--- src/hooks/useMutation.ts
+++ src/hooks/useMutation.ts
@@ -3,10 +3,17 @@ import { Status } from "../types";
 
 interface UseMutationProps<T, Response> {
   mutationFn: (newData: T) => Promise<Response>;
+  // queryKey?: QueryType;
+  onSuccess?: () => Promise<unknown> | void;
+  onMutate?: () => void;
+  onError?: (err?: Error) => void;
 }
 
 export const useMutation = <T, Response>({
   mutationFn,
+  onSuccess,
+  onMutate,
+  onError,
 }: UseMutationProps<T, Response>) => {
   const [data, setData] = useState<Response | null>(null);
   const [status, setStatus] = useState<Status>(Status.init);
@@ -14,23 +21,42 @@ export const useMutation = <T, Response>({
 
   const memoizedMutationFn = useRef(mutationFn);
 
+  const memoizedOnMutate = useRef(onMutate);
+  const memoizedOnSuccess = useRef(onSuccess);
+  const memoizedOnError = useRef(onError);
+
   useLayoutEffect(() => {
+    //mutation fn
     memoizedMutationFn.current = mutationFn;
-  }, [mutationFn]);
+    // onSuccess
+    memoizedOnSuccess.current = onSuccess;
+    //onMutate
+    memoizedOnMutate.current = onMutate;
+    //onError
+    memoizedOnError.current = onError;
+  }, [mutationFn, onSuccess, onMutate, onError]);
 
   const mutate = useCallback((newData: T) => {
     setStatus(Status.loading);
 
+    //onMutate
+    memoizedOnMutate.current?.();
+
+    //mutation fn
     memoizedMutationFn
       .current(newData)
-      .then((data) => {
+      .then(async (data) => {
         setStatus(Status.success);
 
         setData(data);
+
+        await memoizedOnSuccess.current?.();
       })
       .catch((error?: Error) => {
         setError(error);
         setStatus(Status.error);
+
+        memoizedOnError.current?.(error);
       });
   }, []);
 
